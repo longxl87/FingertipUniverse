b109a64628ef1c2d40721497110599b870ed3e70
longxiaolei <longxiaolei@guidefuture.com>
2025-03-10 20:49:18 +0800

    初始化相关的内容

--- fingertip_universe/feature_engine_utils.py
+++ fingertip_universe/feature_engine_utils.py
@@ -107,23 +107,56 @@ def calc_ks(y_true, y_score):
     ks = np.max(np.abs(fpr - tpr))
     return ks
 
-# def univariate(y, x, bins: list = None, n_bins: int = 10, alpha: float = 0.001):
-#     """
-#     使用传入的数据进行分析
-#     :param y:
-#     :param x:
-#     :param bins:
-#     :param n_bins:
-#     :param alpha:
-#     :return:
-#     """
-#     y = np.asarray(y)
-#     if pd.api.types.is_numeric_dtype(x):
-#         x = np.asarray(x, dtype=float)
-#         x_cut_bins = cut_bins(x,n_bins,method='freq')
-#         data = pd.DataFrame({'x':x,'y':y})
-#         data['bin'] = pd.cut(data[x], bins=x_cut_bins,right=True,retbins=False)
-#
-#     else:
-#         raise ValueError('x must be numeric')
 
+def univariate(y, x, n_bins=10, bins=None, alpha=0.1, num_fill=-999.0, cate_fill=''):
+    y = np.array(y, copy=True, dtype=np.float64)
+    if pd.api.types.is_numeric_dtype(x):
+        x = np.array(x, copy=True, dtype=float)
+        if bins is None:
+            bins = cut_bins(x, n_bins, method='freq')
+        data = pd.DataFrame({'x': x, 'y': y})
+        data.fillna(num_fill, inplace=True)
+        data['bin'] = pd.cut(data['x'], bins=bins, right=True, retbins=False)
+        corr = data['x'].corr(data['y'])
+        auc = roc_auc_score(data['y'], data['x'] * -1.0) if corr < 0 else roc_auc_score(data['y'], data['x'])
+    else:
+        data = pd.DataFrame({'x': x, 'y': y})
+        if bins is not None:
+            data['bin'] = data['x'].map(bins).fillna(cate_fill)
+        else:
+            data['bin'] = data['x'].fillna(cate_fill)
+            corr = 0.1
+            auc = 0
+
+    ascending = corr > 0
+
+    br = data['y'].mean()
+    dti = data.groupby('bin', observed=False).agg(
+        bad=('y', 'sum'),
+        cnt=('y', 'count'),
+        brate=('y', 'mean')
+    ).sort_values('bin', ascending=ascending).assign(
+        good=lambda x: x['cnt'] - x['bad'],
+        pct=lambda x: x['cnt'] / (x['cnt'].sum())
+    ).assign(
+        bad_cum=lambda x: x['bad'].cumsum(),
+        good_cum=lambda x: x['good'].cumsum(),
+        bad_prime=lambda x: x['bad'] + alpha,
+        good_prime=lambda x: x['good'] + alpha,
+        lift=lambda x: x['brate'] / br
+    ).assign(
+        brate_cum=lambda x: x['bad_cum'] / (x['bad_cum'] + x['good_cum']),
+        bad_cum_raito=lambda x: x['bad_cum'] / (x['bad'].sum()),
+        good_cum_raito=lambda x: x['good_cum'] / (x['good'].sum()),
+        bad_ratio_prime=lambda x: x['bad_prime'] / (x['bad_prime'].sum()),
+        good_ratio_prime=lambda x: x['good_prime'] / (x['good_prime'].sum()),
+    ).assign(
+        woe=lambda x: np.log(x['bad_ratio_prime'] / x['good_ratio_prime']),
+    ).assign(
+        ks=lambda x: abs(x['bad_cum_raito'] - x['good_cum_raito']),
+        iv=lambda x: (x['bad_ratio_prime'] - x['good_ratio_prime']) * x['woe'],
+        auc=auc
+    ).assign(
+        iv=lambda x: x['iv'].sum()
+    ).reset_index()
+    return dti[['bin', 'bad', 'cnt', 'pct', 'brate', 'brate_cum', 'lift', 'woe', 'ks', 'iv', 'auc']]
