8889ded2bf02186ecbe85e2a9f75d645e1429a4b
longxiaolei <longxiaolei@guidefuture.com>
2025-03-10 21:39:37 +0800

    初始化相关的内容

---
+++ FingertipUniverse/__init__.py
@@ -0,0 +1,20 @@
+from FingertipUniverse.binning_utils import (
+cut_bins,
+make_bin,
+chi2merge
+)
+
+from FingertipUniverse.db_utils import (
+mysql_engine
+)
+
+from FingertipUniverse.feature_engine_utils import (
+calc_auc,
+calc_iv,
+calc_ks,
+calc_psi
+)
+
+from FingertipUniverse.model_utils import (
+prob2score
+)
