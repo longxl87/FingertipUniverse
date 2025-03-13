0436120fe755513e737195e6599b97eb26a2f8a2
longxiaolei <longxiaolei@guidefuture.com>
2025-03-13 14:48:56 +0800

    初始化相关的内容

--- FingertipUniverse/__init__.py
+++ FingertipUniverse/__init__.py
@@ -1,36 +1,41 @@
 from FingertipUniverse.binning_utils import (
-cut_bins,
-make_bin,
-chi2merge
+    cut_bins,
+    make_bin,
+    chi2merge
 )
 
 from FingertipUniverse.db_utils import (
-mysql_engine
+    mysql_engine
 )
 
 from FingertipUniverse.feature_engine_utils import (
-calc_auc,
-calc_iv,
-calc_ks,
-calc_psi,
-univariate
+    calc_auc,
+    calc_iv,
+    calc_ks,
+    calc_psi,
+    univariate
 )
 
 from FingertipUniverse.model_utils import (
-prob2score,
-feature_class,
-model_features,
-oppsite_features,
-oppsite_feature_kfold,
-plot_roc_ks
-)
+    prob2score,
+    feature_class,
+    model_features,
+    oppsite_features,
+    oppsite_feature_kfold,
+    plot_roc_ks)
 
 from time_utils import day_n_of_week
 
-from CONSTANT import (
-    LGB_PARAMS
-)
-from FingertipUniverse.CONSTANT import MON_PARTTEN, DAY_PARTTEN, STANDARD_TIME_PARTTEN, LONG_TIME_PARTTEN
+from FingertipUniverse.CONSTANT import (
+    MON_PARTTEN,
+    DAY_PARTTEN,
+    STANDARD_TIME_PARTTEN,
+    LONG_TIME_PARTTEN)
+
+from FingertipUniverse.file_utils import (
+    data_of_dir,
+    batch_load_data,
+    save_data_to_excel)
 
 import logging
 log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
