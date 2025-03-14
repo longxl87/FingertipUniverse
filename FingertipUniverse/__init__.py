52f5c2e5f2913b1d22ec4e2ce717bc2f9b8bc6a2
longxiaolei <longxiaolei@guidefuture.com>
2025-03-14 11:09:28 +0800

    初始化相关的内容

--- FingertipUniverse/__init__.py
+++ FingertipUniverse/__init__.py
@@ -1,20 +1,34 @@
 from FingertipUniverse.binning_utils import (
     cut_bins,
     make_bin,
-    chi2merge
-)
+    chi2merge)
+
+from FingertipUniverse.CONSTANT import (
+    MON_PARTTEN,
+    DAY_PARTTEN,
+    STANDARD_TIME_PARTTEN,
+    LONG_TIME_PARTTEN)
+
 
 from FingertipUniverse.db_utils import (
-    mysql_engine
-)
+    mysql_engine)
+
+from FingertipUniverse.display_settings import (
+    set_warnings,
+    set_pd_float,
+    set_pd_show)
 
 from FingertipUniverse.feature_engine_utils import (
     calc_auc,
     calc_iv,
     calc_ks,
     calc_psi,
-    univariate
-)
+    univariate)
+
+from FingertipUniverse.file_utils import (
+    data_of_dir,
+    batch_load_data,
+    save_data_to_excel)
 
 from FingertipUniverse.model_utils import (
     prob2score,
@@ -24,18 +38,8 @@ from FingertipUniverse.model_utils import (
     oppsite_feature_kfold,
     plot_roc_ks)
 
-from FingertipUniverse.time_utils import day_n_of_week
-
-from FingertipUniverse.CONSTANT import (
-    MON_PARTTEN,
-    DAY_PARTTEN,
-    STANDARD_TIME_PARTTEN,
-    LONG_TIME_PARTTEN)
-
-from FingertipUniverse.file_utils import (
-    data_of_dir,
-    batch_load_data,
-    save_data_to_excel)
+from FingertipUniverse.time_utils import (
+    day_n_of_week)
 
 import logging
 log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
